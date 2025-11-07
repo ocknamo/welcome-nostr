c684e91bbda1eb22ceae4b5a52b42e286536b3da
ocknamo <s14pes@gmail.com>
2025-11-08 01:44:44 +0900

    feat: page fixed image

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -35,7 +35,8 @@ export default defineUserConfig({
     }),
     seoPlugin({
       hostname: 'ocknamo.github.io',
-      fallBackImage: 'https://ocknamo.github.io/welcome-nostr/ogp-card.jpg', // fix me
+      fallBackImage: "https://ocknamo.github.io/welcome-nostr/ogp-card.jpg", // fix me
+      ogp: (page) => ({ ...page, "og:image":"https://ocknamo.github.io/welcome-nostr/ogp-card.jpg" })
     })
   ],
   theme: defaultTheme({
