998614d46c04e35b55c93c1f8c3481ea33b77275
ocknamo <s14pes@gmail.com>
2025-11-10 22:57:41 +0900

    feat: ogp dir

--- docs/.vuepress/plugins/gemerate-ogp/generate-ogp.ts
+++ docs/.vuepress/plugins/gemerate-ogp/generate-ogp.ts
@@ -8,7 +8,7 @@ export const ogpGeneratorPlugin = () => ({
   name: 'ogp-generator',
   async onGenerated(app) {
     const pages = app.pages
-    const outDir = app.dir.dest()
+    const outDir = app.dir.dest("ogp")
     fs.mkdirSync(outDir, { recursive: true })
 
     // fontデータの読み取り
